e83aea7d09b06e9f16d9504053d47774b345e9ff
Alex Davis <alex2006hw@gmail.com>
2016-01-21 01:12:56 -0800

    inital-lessino

---
+++ js/routes/AppHomeRoute.js
@@ -0,0 +1,9 @@
+import Relay from 'react-relay';
+
+export default class extends Relay.Route {
+  static path = '/';
+  static queries = {
+    game: () => Relay.QL `query { game }`,
+  };
+  static routeName = 'AppHomeRoute';
+}
